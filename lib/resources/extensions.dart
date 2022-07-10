e3090ac2578611d1d65221d869465a7ad5a2ce2a
ra108756@uem.br <locacao@C02GR4B8Q05D.local>
2022-07-10 16:39:47 -0300

    removing desktop folder

--- lib/resources/extensions.dart
+++ lib/resources/extensions.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:visibility_detector/visibility_detector.dart';
 
 extension ContextExt on BuildContext {
   TextTheme get themeData => Theme.of(this).textTheme;
-}
-
-extension VisibilityExt on VisibilityInfo {
-  bool isVisible([double portion = 98]) => (visibleFraction * 100) < portion;
 }
