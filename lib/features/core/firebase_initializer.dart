ef26f0506878d71d507a1d89ba15dbdc9e66c2df
ra108756@uem.br <locacao@C02GR4B8Q05D.local>
2022-07-10 18:55:32 -0300

    fixing firebase wbe

--- lib/features/core/firebase_initializer.dart
+++ lib/features/core/firebase_initializer.dart
@@ -1,18 +1,9 @@
 import 'package:bloc/bloc.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:portfolio_website/firebase_options.dart';
 
 class FirebaseInitializerCubit extends Cubit<bool> {
   FirebaseInitializerCubit() : super(false);
 
   void initializeApp() async {
-    await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform)
-      ..setAutomaticDataCollectionEnabled(true)
-      ..setAutomaticResourceManagementEnabled(true);
-    await FirebaseAnalytics.instance.setUserId();
-
     emit(true);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_analytics_web/firebase_analytics_web.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:portfolio_website/features/core/firebase_initializer.dart';
 import 'package:portfolio_website/features/core/presentation/cubits/control_page_cubit.dart';
 import 'package:portfolio_website/features/splash/presentation/pages/splash_page.dart';
+import 'package:portfolio_website/firebase_options.dart';
 import 'package:portfolio_website/resources/theme.dart';
 
 void main() {
@@ -20,6 +23,15 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    initializeFirebase();
+  }
+
+  void initializeFirebase() async {
+    final app = await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    FirebaseAnalyticsWeb(app: app).logEvent(name: 'New access!');
   }
 
   @override
